1d026f6cd2ac3249f1c61d29609917425f383d2b
oshimmmm <yasuhirooshima0419@gmail.com>
2024-12-02 06:56:41 +0900

    Authentication追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { FormalinProvider } from './context/FormalinContext';
 import Header from './components/Header';
 import Home from './pages/Home';
@@ -7,8 +8,12 @@ import Ingress from './pages/Ingress';
 import Egress from './pages/Egress';
 import Submission from './pages/Submission';
 import List from './pages/List';
+import { auth } from './firebase';
+import Login from './pages/Login';
 
 const App: React.FC = () => {
+  const [user] = useAuthState(auth);
+  
   return (
     // エントリポイントであるApp.tsxで、全てのコンポーネントをラッピング
     // ラッピングされた子コンポーネントは、コンテキストのホルマリンリスト情報やデータ追加、更新の機能が使える
@@ -16,11 +21,14 @@ const App: React.FC = () => {
       <Router>
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/ingress" element={<Ingress />} />
-          <Route path="/egress" element={<Egress />} />
-          <Route path="/submission" element={<Submission />} />
-          <Route path="/list" element={<List />} />
+          {/* ログインしていない場合はログイン画面にリダイレクト */}
+          <Route path="/login" element={<Login />} />
+          
+          <Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
+          <Route path="/ingress" element={user ? <Ingress /> : <Navigate to="/login" />} />
+          <Route path="/egress" element={user ? <Egress /> : <Navigate to="/login" />} />
+          <Route path="/submission" element={user ? <Submission /> : <Navigate to="/login" />} />
+          <Route path="/list" element={user ? <List /> : <Navigate to="/login" />} />
         </Routes>
       </Router>
     </FormalinProvider>
